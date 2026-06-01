a313f3e700f0146b4206cec7664e8a4bad816b31
Dane Albaugh <dane@augno.com>
2026-06-01 15:24:34 -0400

    Build SDK
    Stainless-Generated-From: 02fdee9c21e747f730cba6a79d0919eb3ab0bebf

--- scripts/utils/make-dist-package-json.cjs
+++ scripts/utils/make-dist-package-json.cjs
@@ -13,9 +13,18 @@ for (const key of ['types', 'main', 'module']) {
   if (typeof pkgJson[key] === 'string') pkgJson[key] = pkgJson[key].replace(/^(\.\/)?dist\//, './');
 }
 
+delete pkgJson.private;
 delete pkgJson.devDependencies;
 delete pkgJson.scripts.prepack;
 delete pkgJson.scripts.prepublishOnly;
 delete pkgJson.scripts.prepare;
+delete pkgJson.scripts.release;
+
+if (pkgJson.publishConfig) {
+  delete pkgJson.publishConfig.directory;
+}
+
+// The dist tarball is published from dist/, so include all built artifacts.
+pkgJson.files = ['**/*', '!.npmrc'];
 
 console.log(JSON.stringify(pkgJson, null, 2));
